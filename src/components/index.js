f5e3df62902f3d2277ed9280c61c1befa454e06f
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-20 15:12:51 -0300

    feat(components): add index to export components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { AddButton } from './AddButton'
+export { DropdownContainer } from './DropdownContainer'
+export { ColorCircle } from './ColorCircle'
+export { ColorCircleContainer } from './ColorCircleContainer'
+export { Label } from './Label'
+export { Title } from './Title'
