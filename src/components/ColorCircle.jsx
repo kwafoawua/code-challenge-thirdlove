c23d754840129647cdc210ab8d10ed64fab739c6
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-20 15:12:23 -0300

    feat(colorCircle component): initial implementation of ColorCircle and ColorCircleContainer component

---
+++ src/components/ColorCircle.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+import { colors } from '../styles'
+
+export const ColorCircle = styled.button`
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  border: 0px;
+  background-color: ${props => props.color};
+  cursor: pointer;
+  outline: none;
+  
+  &:focus {
+    border: solid 3px ${colors.gray};
+  }
+`
---
+++ src/components/ColorCircleContainer.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+
+export const ColorCircleContainer = styled.div`
+  width: 250px;
+  display: inline-flex;
+  justify-content: space-between;
+`
