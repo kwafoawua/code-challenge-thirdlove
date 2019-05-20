9e818a4a7596f26ce2255dd2584b966ed4ab4b8b
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-20 15:08:02 -0300

    chore(styles): add naked colors and fonts

---
+++ src/styles.js
@@ -0,0 +1,16 @@
+export const colors = {
+  'lightGray': '#e0e0e0',
+  'darkGray': '#222222',
+  'gray': '#c9c9c9',
+  'naked1': '#ffefe5',
+  'naked2': '#dab5a1',
+  'naked3': '#d49d89',
+  'naked4': '#6a3a33',
+  'naked5': '#524346',
+}
+
+export const fonts = {
+  'primary': "'Open Sans', sans-serif",
+  'title': "'Abhaya Libre', serif",
+  'description': "'Montserrat', sans-serif",
+}
