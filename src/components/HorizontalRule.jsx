55e21a9cc23d6c24320168d4c669e43575683132
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-23 15:53:47 -0300

    feat(HorizontalRule component): initial implementation of HR component

---
+++ src/components/HorizontalRule.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import { colors } from '../common'
+
+export const HorizontalRule = styled.hr`
+  height: 3px;
+  width: 100%;
+  background-color: ${colors.hr};
+  border: none;
+  grid-area: description;
+`
