2f949bce73efc8cc3bb72c00a82aa4ba34965b2e
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-23 15:50:55 -0300

    feat(Label component): change darkGray to title

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled'
-import { colors, fonts } from '../styles'
+import { colors, fonts } from '../common'
 
 export const Label = styled.div`
   display: inline-flex;
   font-family: ${fonts.primary};
   font-size: 13px;
   letter-spacing: 1.5px;
-  color: ${colors.darkGray};
+  color: ${colors.title};
   font-weight: ${props => props.bold ? 600 : 'normal'};
   text-transform: uppercase;
   margin-bottom: 5px;
