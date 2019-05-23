f2cb3c418ed7e987fa7cdbd7e03e23c0440e6e03
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-23 15:57:18 -0300

    feat(storybook): add new Components including fullPage Container

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react'
 import { storiesOf } from '@storybook/react'
 import { Button } from '@storybook/react/demo'
-import { AddButton, DropdownContainer, ColorCircle, ColorCircleContainer,
-  Label, Title } from "../components/index"
-import { colors } from '../styles'
+import { AddButton, DropdownContainer, ColorCircle, ColorCircleContainer, Label, Title, Price,
+  HorizontalRule, Container, InnerContainer } from "../components/index"
+import { colors } from '../common'
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -40,4 +40,65 @@ storiesOf('Button', module)
   ))
   .add('Title', () => (
     <Title>24/7 Classic Perfect Coverage Bra</Title>
+  ))
+  .add('Price', () => (
+    <Price>$68</Price>
+  ))
+  .add('HorizontalRule', () => (
+    <HorizontalRule/>
+  ))
+  .add('Container', () => (
+    <Container>
+      <Title>24/7 Classic Perfect Coverage Bra</Title>
+      <Price>$68</Price>
+      {/* eslint-disable-next-line max-len */}
+      <img src="http://static.thirdlove.com/frontend-challenge/black-lace-plunge/Black_LacePlunge_Black_LaceCheeky_HYB_NY_2018_03_079_1000x.jpg"/>
+      <InnerContainer display="inline-flex">
+        <Label>Color: </Label><Label bold>Naked</Label>
+      </InnerContainer>
+      <ColorCircleContainer>
+        <ColorCircle tabindex="1" color={colors.naked1}/>
+        <ColorCircle tabindex="2" color={colors.naked2}/>
+        <ColorCircle tabindex="3" color={colors.naked3}/>
+        <ColorCircle tabindex="4" color={colors.naked4}/>
+        <ColorCircle tabindex="5" color={colors.naked5}/>
+      </ColorCircleContainer>
+      <InnerContainer display="inline-flex">
+        <Label>Stock: </Label><Label bold>20</Label>
+      </InnerContainer>
+      <InnerContainer display="inline-flex">
+        <DropdownContainer title="Band Size">
+          <option>1</option>
+          <option>2</option>
+        </DropdownContainer>
+        <DropdownContainer title="Cup Size">
+          <option>1</option>
+          <option>2</option>
+        </DropdownContainer>
+      </InnerContainer>
+      <AddButton>Add to Bag</AddButton>
+      <Label>Details</Label>
+      <HorizontalRule/>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate veli.</p>
+      <ul>
+        <li>
+          Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+          nisi ut aliquid ex ea commodi exercitationem ullam corporis suscipit laboriosam, nisi ut
+          aliquid ex ea commodi.
+        </li>
+        <li>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+          voluptatum deleniti atq exercitationem ullam corporis suscipit laboriosam, nisi ut
+          aliquid ex ea commodi.
+        </li>
+        <li>
+          Ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu exercitationem
+          ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.
+        </li>
+      </ul>
+
+    </Container>
   ))
