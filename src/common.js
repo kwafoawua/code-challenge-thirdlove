c921afd0a87348714a57e1b66b935ebdf4958436
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-23 15:39:14 -0300

    chore(styles): change name to common and add device sizes

--- src/common.js
+++ src/common.js
@@ -1,7 +1,10 @@
 export const colors = {
-  'lightGray': '#e0e0e0',
-  'darkGray': '#222222',
-  'gray': '#c9c9c9',
+  'dropdownBorder': '#e0e0e0',
+  'title': '#222222',
+  'circleBorder': '#c9c9c9',
+  'description': '#444444',
+  'price': '#666666',
+  'hr': '#f7f7f7',
   'naked1': '#ffefe5',
   'naked2': '#dab5a1',
   'naked3': '#d49d89',
@@ -13,4 +16,9 @@ export const fonts = {
   'primary': "'Open Sans', sans-serif",
   'title': "'Abhaya Libre', serif",
   'description': "'Montserrat', sans-serif",
+}
+export const sizes = {
+  'mobile': '376px',
+  'tablet': '769px',
+  'desktop': '1440px',
 }
