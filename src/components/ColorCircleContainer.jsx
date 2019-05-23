a8680de3d22d4a541d7794df7b62eefc66a83675
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-23 15:42:54 -0300

    feat(ColorCircleContainer component): add margin bottom

--- src/components/ColorCircleContainer.jsx
+++ src/components/ColorCircleContainer.jsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled'
 
 export const ColorCircleContainer = styled.div`
-  width: 250px;
   display: inline-flex;
   justify-content: space-between;
+  width: 250px;
+  margin-bottom: 25px;
+
 `
