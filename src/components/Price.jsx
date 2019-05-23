fa3c188f7d76af477e3f87d485266bc0dada4bcd
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-23 15:52:51 -0300

    feat(Price component): initial implementation of Price component

---
+++ src/components/Price.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+import { colors, fonts } from '../common'
+
+export const Price = styled.div`
+  grid-area: price;
+  text-align: center;
+  font-size: 15px;
+  font-weight: 600;
+  font-family: ${fonts.primary};
+  color: ${colors.price};
+  letter-spacing: 1px;
+  justify-self: center;
+
+
+`
