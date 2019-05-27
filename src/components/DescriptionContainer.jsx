ce4b27ae00a7dead8591041d3630a13f31bd8363
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-27 20:34:31 -0300

    refactor(DescriptionContainer component): add media query

--- src/components/DescriptionContainer.jsx
+++ src/components/DescriptionContainer.jsx
@@ -1,5 +1,16 @@
 import styled from '@emotion/styled'
+import { sizes } from '../common'
 
 export const DescriptionContainer = styled.div`
   grid-area: description;
+  margin-top: 39px;
+
+ @media(min-width: ${sizes.mobile}) {
+  margin-bottom: 53px;
+ }
+  
+ @media(min-width: ${sizes.tablet}) {
+  margin-top: 35px;
+  margin-bottom: 79px;
+ }
 `
