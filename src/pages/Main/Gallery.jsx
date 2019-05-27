d7c85e8bc9d5b26e5a7ade150a6729cf94366c02
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-27 20:45:04 -0300

    feat(Gallery Component): add component that shows the images

---
+++ src/pages/Main/Gallery.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import {
+  BigImageGallery,
+  CarouselGallery,
+  PhotoGalleryContainer,
+  VerticalGallery,
+} from '../../components'
+
+const PREFIX = 'http://'
+
+class Gallery extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      width: window.innerWidth,
+      selectedImage: '',
+      images:[],
+    }
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange)
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (this.props.images !== nextProps.images) {
+      this.setState({images: nextProps.images})
+      this.setState({selectedImage : PREFIX+nextProps.images[0].src600})
+    }
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth })
+  }
+
+  handleClickImage = (imageSet) => {
+    console.log(imageSet.src600)
+  let selectedImage = PREFIX+imageSet.src600
+    this.setState({selectedImage} )
+  }
+
+  render() {
+    const isDesktop = this.state.width >= 769
+
+    if (isDesktop) {
+      return (
+        <PhotoGalleryContainer>
+          <VerticalGallery>
+            {
+              this.state.images.map((imageSet, index) => {
+                return (
+                  <img
+                    src={PREFIX+imageSet.src100}
+                    key={imageSet.src100}
+                    tabIndex={index}
+                    onClick={() => this.handleClickImage(imageSet)}/>
+                )
+              })
+            }
+          </VerticalGallery>
+          <BigImageGallery src={this.state.selectedImage} />
+        </PhotoGalleryContainer>
+      )
+    } else {
+      return (
+        <CarouselGallery>
+          {
+            this.state.images.map((imageSet) => {
+              if(this.state.width < 376){
+                return(<img src={PREFIX+imageSet.src600} key={imageSet.src600}/>)
+              } else {
+                return (<img src={PREFIX+imageSet.src1000} key={imageSet.src1000}/>)
+              }
+            })
+          }
+        </CarouselGallery>
+      )
+    }
+  }
+}
+
+export default Gallery
