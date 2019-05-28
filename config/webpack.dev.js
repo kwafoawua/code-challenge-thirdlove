e1990d587756b68f4955f00a08ab598a69a29f48
Afua Kwafo-Awua <afua.kw@gmail.com>
2019-05-27 23:04:40 -0300

    chore(webpack): add merge.smart

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge')
 const path = require('path')
 const common = require('./webpack.common.js')
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   plugins: [
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const common = require('./webpack.common.js')
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
   mode: 'production',
   optimization: {
     minimizer: [ new UglifyJsPlugin() ],
